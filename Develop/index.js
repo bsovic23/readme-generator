bdec72409ebf6be4a9068bfd86f26cea1593e84b
Brit Sovic <bsovic@tulane.edu>
2022-12-23 13:41:54 -0500

    project setup

--- Develop/index.js
+++ Develop/index.js
@@ -4,6 +4,13 @@ const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+/*
+Potential questions to include above
+1. What is the project name
+2. What languages were used 
+
+*/
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
